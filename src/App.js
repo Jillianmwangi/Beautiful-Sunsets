1b30d43dcca0ac8f2d86058b187f8f4f6c5b58f4
Jillianmwangi <mwangyjill@gmail.com>
2023-02-02 15:00:16 +0300

    add sunset photos

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Sunset from './Sunset'
+
+function App() {
+  return (
+    <>
+    
+    <Sunset/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Sunset.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Sunset = () => {
+    return(
+        <div class="prettysuns">
+        <h1 class="sunphotos">Beautiful Sunsets</h1>
+        <div class="photos">
+           <img src="../images/sunset.jpeg" alt="Sunsets" srcset=""/>
+           <img src="../images/best.jpeg" alt="Mountain And Sunset" srcset=""/>
+           <img src="../images/crimson.jpeg" alt="Palms and sunsets" srcset=""/>
+           <img src="../images/oceanic.jpeg" alt="Rocky Mountain Sunset" srcset=""/>
+           <img src="../images/oceans.jpeg" alt="The Scenic Alps" srcset=""/>
+           <img src="../images/palmsun.jpeg" alt="Sunset Over Water" srcset=""/>
+        </div>
+        </div>
+    )
+    }
+export default Sunset;
+
+
+
+
